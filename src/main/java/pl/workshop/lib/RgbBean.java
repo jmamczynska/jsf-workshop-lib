ccf89937c7d2a5fe8eb06b2629c88254d03e6451
Joanna Mamczyńska <jma@earp.pl>
2016-07-09 12:09:45 +0200

    Warsztaty 2 - RGB composite component.

--- src/main/java/pl/workshop/lib/RgbBean.java
+++ src/main/java/pl/workshop/lib/RgbBean.java
@@ -10,5 +10,28 @@ import javax.inject.Named;
 public class RgbBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	private int r;
+	private int g;
+	private int b;
+	
+	public int getR() {
+		return r;
+	}
+	public void setR(int r) {
+		this.r = r;
+	}
+	public int getG() {
+		return g;
+	}
+	public void setG(int g) {
+		this.g = g;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
 
 }
