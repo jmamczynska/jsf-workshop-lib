a5046bd91737017e796d30344f0047d45c308ff2
Joanna Mamczyńska <jma@earp.pl>
2016-09-29 11:12:47 +0200

    Product 5 code fixup

--- src/main/java/pl/workshop/lib/LocationData.java
+++ src/main/java/pl/workshop/lib/LocationData.java
@@ -34,7 +34,7 @@ public class LocationData implements Serializable {
 		districts.add(new District(0, "opolskie"));
 		districts.add(new District(1, "dolnoœl¹skie"));
 		districts.add(new District(2, "œl¹skie"));
-		districts.add(new District(3, "pomorskie"));
+		districts.add(new District(3, "pomorskie1"));
 		districts.add(new District(4, "mazowieckie"));
 
 		allCities = new ArrayList<>();
