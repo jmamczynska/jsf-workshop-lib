f144948a43cfd54b13aec211c1fb1c0e51cb332c
Joanna Mamczyńska <jma@earp.pl>
2016-09-29 09:54:05 +0200

    New product added

--- src/main/java/pl/workshop/lib/ProductController.java
+++ src/main/java/pl/workshop/lib/ProductController.java
@@ -1,5 +1,7 @@
 package pl.workshop.lib;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -9,8 +11,6 @@ import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import java.io.Serializable;
-import java.math.BigDecimal;
 
 @Named
 @SessionScoped
@@ -27,6 +27,7 @@ public class ProductController implements Serializable {
 		productList.add(new Product(1, "AA", "Pendrive 16GB", new BigDecimal("32.40"), new Date()));
 		productList.add(new Product(2, "BB", "S³uchawki", new BigDecimal("55.47"), new Date()));
 		productList.add(new Product(3, "CC", "Pendrive 32GB", new BigDecimal("87.20"), new Date()));
+		productList.add(new Product(4, "DD", "Pendrive 32GB", new BigDecimal("81.20"), new Date()));
 	}
 
 	public Product getProduct() {
